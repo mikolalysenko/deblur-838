2114fc33f4b9a3d9c9c2b73d1ff094d189b0f488
mikolalysenko <mikolalysenko@4448e800-f6fa-11dd-ada4-5153b8187bf2>
2009-02-11 01:46:49 +0000

    All major functions implemented.  Need to debug though.

--- deriv_psf.m
+++ deriv_psf.m
@@ -1,15 +1,16 @@
 %This function constructs the kernel for the derivative wrt x,y
 function [res] = deriv_psf( w, h, dx, dy )
 
-arr = [[1]];
-
+%Construct derivative maps incrementally
+arr = [1];
 for i=1:dx
+    arr = conv2(arr, [1,-1]);
 end
-
 for j=1:dy
+    arr = conv2(arr, [1;-1]);
 end
 
 %Done
-res = psf2otf(arr);
+res = psf2otf(arr, [w,h]);
 
 end
