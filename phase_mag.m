360d129eede7281236e6d008df132bcd3c61dd8e
mikolalysenko <mikolalysenko@4448e800-f6fa-11dd-ada4-5153b8187bf2>
2009-02-15 04:48:07 +0000

    Fixed the magnitude function.

--- phase_mag.m
+++ phase_mag.m
@@ -3,15 +3,21 @@
 function [ res ] = phase_mag( image )
 w = size(image,1);
 h = size(image,2);
+s = sqrt(w * h);
 
-tmp = zeros(w, h);
-for dx=0:2
-for dy=0:2
-    D = conj(fft2(image)) .* deriv_psf(w, h, dx, dy);
-    tmp = tmp + weight_func(dx, dy) .* conj(D) .* D;
+%Compute f_phase (the triangle operator)
+f_phase = zeros(w, h);
+for d=0:2
+for dx=0:d
+    dy = d-dx;
+    fd = deriv_psf(w, h, dx, dy);
+    f_phase = f_phase + weight_func(dx, dy) .* conj(fd) .* fd;
 end
 end
+f_phase = f_phase ./ s;
 
-tmp = ifft2(tmp);
+%Compute rescaled magnitude
+tmp = conj(fft2(image)) .* tmp ./ s;
 res = sum(sum(conj(tmp) .* tmp));
+
 end
